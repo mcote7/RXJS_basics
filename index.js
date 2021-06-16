9ce547fde40a52ac9b8893fc29458b3eab616f65
Michael Cote <michaelcote7@outlook.com>
2021-06-15 20:31:29 -0700

    .

--- index.js
+++ index.js
@@ -263,3 +263,83 @@
 // keycodeWithPluck$.subscribe(console.log);
 
 
+// begin lesson code
+// import { from, interval } from 'rxjs';
+// import { reduce, take } from 'rxjs/operators';
+
+// const numbers = [1, 2, 3, 4, 5];
+
+/*
+ * Reducer functions take the accumulated value (last return) 
+ * and current value, returning a new accumulated value. 
+ * You can think of this like a snowball rolling downhill, 
+ * collecting values over time.
+ */
+// const totalReducer = (accumulator, currentValue) => {
+//   return accumulator + currentValue;
+// };
+
+/*
+ * Our reducer function is invoked on each emission and the accumulated
+ * value stored. On completion the current accumulated value is emitted.
+ * In this example we are supplying a seed value (initial accumulator) of 0
+ * as the second parameter.
+ */
+// from(numbers)
+//   .pipe(reduce(totalReducer, 0))
+//   .subscribe(console.log);
+
+// interval(1000)
+//   .pipe(
+    /*
+     * Important! reduce only emits one value, the final accumulated value
+     * on completion. We are forcing completion by using the take operator.
+     * If you want to emit each new accumulated value, you will use the scan
+     * operator, which is the focus of the next lesson.
+     */
+  //   take(3),
+  //   reduce(totalReducer, 0)
+  // )
+  // .subscribe({
+  //   next: console.log,
+  //   complete: () => console.log('Complete!')
+  // });
+
+  import './style.css';
+console.clear();
+
+// begin lesson code
+import { interval } from 'rxjs';
+import { scan, mapTo, filter } from 'rxjs/operators';
+
+// elem refs
+const countdown = document.getElementById('countdown');
+const message = document.getElementById('message');
+
+// streams
+const counter$ = interval(1000);
+
+/*
+ * Starting countdown example. In future lessons we will learn
+ * about how to seed our countdown, complete when the timer hits zero, 
+ * pause the countdown, and resume. More to come!
+ */
+counter$
+  .pipe(
+    // since we have learned about mapTo, let's use it!
+    mapTo(-1),
+    scan((accumulator, current) => {
+      return accumulator + current;
+    }, 10),
+    // prevent entering the negatives
+    filter(value => value >= 0)
+  )
+  .subscribe((value) => {
+    // for every emitted value, update our countdown display
+    countdown.innerHTML = value;
+    if (!value) {
+      message.innerHTML = 'Liftoff!';
+    }
+  });
+
+  
