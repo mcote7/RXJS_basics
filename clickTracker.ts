f5eb172692d709bcd6a9addc9c94b37f53111f62
Michael Cote <michaelcote7@outlook.com>
2021-06-26 12:57:00 -0700

    .

---
+++ clickTracker.ts
@@ -0,0 +1,11 @@
+import { fromEvent } from "rxjs";
+import { sampleTime, map } from "rxjs/operators";
+
+const click$ = fromEvent<MouseEvent>(document, 'click');
+
+click$.pipe(
+  sampleTime(4000),
+  map(({clientX, clientY})=> ({
+    clientX, clientY
+  }))
+).subscribe(console.log);
