4d8e5ee674732f246aa0b18e3b6ad2064a3d52e0
Michael Cote <michaelcote7@outlook.com>
2021-06-26 12:07:51 -0700

    .

--- scrollProgress.ts
+++ scrollProgress.ts
@@ -2,7 +2,7 @@ console.clear();
 
 // begin lesson code
 import { fromEvent } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, throttleTime, tap } from 'rxjs/operators';
 
 /*
  * Calculate progress based on scroll position
@@ -23,8 +23,11 @@ const progress$ = scroll$.pipe(
   /*
    * For every scroll event, we use our helper function to 
    * map to a current scroll progress value.
+   * THROTTLE TIME reduces the amount of time the function is called
    */
-  map(({ target }: any) => calculateScrollPercent(target.scrollingElement))
+  throttleTime(30),
+  map(({ target }: any) => calculateScrollPercent(target.scrollingElement)),
+  tap(console.log) 
 );
 /*
  * We can then take the emitted percent and set the width
